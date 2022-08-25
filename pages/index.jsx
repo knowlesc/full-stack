cd828d77ef26a0080913ca8c288c901533ac97cc
knowlesc <knowlesc8@gmail.com>
2022-08-25 11:38:30 -0700

    Add form elements

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,10 +3,32 @@ import PropTypes from 'prop-types';
 import { Layout } from '../components/Layout';
 import { PostShape } from '../prop-shapes/post';
 import { fetchPosts } from '../prisma/helpers/post';
+import { useState } from 'react';
 
 const Journal = ({ posts }) => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    console.log('submit', title, content);
+  };
+
   return (
     <Layout>
+      <h1>New Post</h1>
+      <form onSubmit={submitForm}>
+        <label htmlFor="title">Title</label>
+        <input
+          name="title"
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label htmlFor="content">Content</label>
+        <textarea name="content" onChange={(e) => setContent(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+
       <h1>Posts</h1>
       {/* todo: render posts in a user-friendly way */}
       <pre>{JSON.stringify(posts, null, 2)}</pre>
